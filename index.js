4e4cf38240f4808614120d1486a2b7e2886dd7d5
Mayur Pawar <pawarmr9999@gmail.com>
2023-12-21 23:06:14 +0530

    dec 21 2023

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-// let count = 0;
+//
 
-// function increment() {
-//   count += 1;
-//   return console.log(count);
-// }
-// count = 99
-
-// increment();
-// increment();
-// increment();
-
-function increment() {
-  let count = 0;
-  let clouser = () => {
-    count += 1;
-    return count;
-  };
-  // console.log(count)
-  return clouser;
-}
-console.log()
-
-increment();
-increment();
-increment();
+const { hello } = require("./index2.js");
+hello();
+// console.log(hello())
